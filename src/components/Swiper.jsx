f3bf975091c1527d65b2844223625df5137cddd9
oladapo <oladapoelijah49@gmail.com>
2024-05-08 16:14:11 +0100

    swish

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -36,8 +36,6 @@ export default function App() {
             </div>
           </a>
         </SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
       </Swiper>
     </>
   );
